61576a2d1b3d826b5d92d8772a339f5d848f59ca
christoffel <c.toffel9@gmail.com>
2020-10-22 19:16:20 +0700

    Update

--- Keep Breathing/Assets/Scripts/chestController.cs
+++ Keep Breathing/Assets/Scripts/chestController.cs
@@ -7,21 +7,51 @@ public class chestController : MonoBehaviour
     public bool isOpen;
     public Animator anim;
     private inventorySystem inventory;
+    public GameObject itemButton;
+    bool inventOn;
     public void openChest()
     {
         if (!isOpen)
         {
-            isOpen = true;
-            Debug.Log("Chest is now open...");
+            if (inventOn)
+            {
+                isOpen = true;
+                Debug.Log("Chest is now open...");
+                for (int i = 0; i < inventory.slots.Length; i++)
+                {
+                    if (inventory.isFull[i] == false)
+                    {
+                        inventory.isFull[i] = true;
+                        Instantiate(itemButton, inventory.slots[i].transform);
+                        break;
+
+                    }
+
+                }
+            }
+            else
+            {
+                isOpen = false;
+                Debug.Log(" There are no Inventory");
+            }
 
         }
     }
 
     private void Update()
     {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<inventorySystem>();
         if (isOpen == true)
         {
             anim.SetBool("isOpen", true);
+        }
+        if (inventory != null)
+        {
+            inventOn = true;
+        }
+        else
+        {
+            inventOn = false;
         }
     }
 
