67df081c46602ca63acdba9571e17c128bbdd2d2
christoffel <c.toffel9@gmail.com>
2020-11-24 09:59:42 +0700

    update

---
+++ Keep Breathing/Assets/seeker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seeker : MonoBehaviour
+{
+    public Transform state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject objToSpawn;
+        objToSpawn = new GameObject("AnjayBisa");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Transform child in state) if (child.CompareTag("maumere"))
+            {
+                Debug.Log("Found");
+            }
+    }
+}
