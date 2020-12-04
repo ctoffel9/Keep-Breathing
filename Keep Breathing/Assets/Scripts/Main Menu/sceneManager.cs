15a8220cc866527adb14e3121d8b71188e8b79b9
Ihzam Chaidir Aldy <ihzam.aldy27@gmail.com>
2020-12-04 10:51:39 +0700

    Update Sound
    udah ku masukkin semua fell, mulai dari hover button & click... tapi untuk scene Select Chapter blom fell. mau dirubah tapi ntar di setting kgk bisa dipake tuh option

--- Keep Breathing/Assets/Scripts/Main Menu/sceneManager.cs
+++ Keep Breathing/Assets/Scripts/Main Menu/sceneManager.cs
@@ -5,20 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class sceneManager : MonoBehaviour
 {
+	public AudioManager audioManager;
     public GameObject optionsPanel;
+
+    void Start(){
+    	audioManager = AudioManager.instance;
+        if(audioManager == null){
+            Debug.LogError("AudioManager tidak ada dalam Scene ini");
+        }
+    }
+
     public void newGame()
     {
         SceneManager.LoadScene("Intro");
+
     }
 
     public void level1()
     {
         SceneManager.LoadScene("Level 1");
+        audioManager.Play("BGM in Game");
     }
 
     public void options()
     {
-        optionsPanel.SetActive(true);   
+        optionsPanel.SetActive(true);  
     }
 
     public void closeoptions()
@@ -34,5 +45,13 @@ public class sceneManager : MonoBehaviour
     public void MainMenu()
     {
         SceneManager.LoadScene("Main Menu");
+    }
+
+    public void ButtonHover(){
+        audioManager.Play("hover");
+    }
+
+    public void ButtonClick(){
+    	audioManager.Play("click");
     }
 }
